2607e7052fefdd824a77f68ca573cb6f79b9dd46
Evan E <evan.escabasse@gmail.com>
2023-04-04 20:01:19 +0200

    complete rewrite

---
+++ README.md
@@ -0,0 +1,23 @@
+# ArcadiaHome
+A simple home menu for our highschool "chef d'oeuvre" project "Arcadia" 
+
+What is Arcadia ? Arcadia is a "Chef d'oeuvre" project we have to do in our highschool. 
+We (me and 2 other people as a group) have to do a arcade and i'm doing the basic Home menu.
+---------------------------------------------------------------
+
+The Menu is written in Python. There is 3 buttons to choose between 3 games.
+  - Mario bros for NES
+  - Pac-Man for NES
+  - Undetermined game
+  
+---------------------------------------------------------------
+
+//TO DO
+  - compatibility with arcade buttons
+  - Launch game
+  - Re-made backghround image
+  - Integrate it in the raspberry for the arcade
+  - ~have a 20/20~
+  - Integrate the game in the raspberry to be able to launch them with the menu
+  
+---------------------------------------------------------------
--- home.py
+++ home.py
@@ -1,72 +1,100 @@
-from email.mime import image
-from inspect import Attribute
-from tkinter import *
-from tkinter import ttk
-from tkinter.tix import IMAGE
-from turtle import position, right
-from PIL import Image,ImageTk
-import os, webbrowser 
+import pygame
+from pygame import mixer
 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
+# Initialize Pygame
+pygame.init()
 
-#Array game
-GameName = ["PacManBTN", "BackroomBTN", "MarioBrosBTN"]
-GamePath = ["", "", ""]
-GameIMAG = ["PacMan.png", "", "smbNES.jpg"]
+#starting the mixer for sfx and add background music
+mixer.init()
+mixer.music.load('music.wav')
+mixer.music.play(-1)
 
-#Command :
-def pacmancmd():
-   webbrowser.open('https://google.com')
-def backroomcmd():
-   webbrowser.open('https://google.com')
-def mariocmd():
-   webbrowser.open('https://google.com')
+# Set the width and height of the screen [width, height]
+size = (1920, 1080)
+screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 
+# Set the caption for the window
+pygame.display.set_caption("Arcadia")
 
-rotationactive = 0
+# Load the background image and scale it to the size of the screen
+background_image = pygame.image.load("backgroundImage.ppm").convert()
+background_image = pygame.transform.scale(background_image, size)
 
-#Configuration windows et BG:
-root = Tk()
-#root.geometry("650x250")
-root.title("Arcadia Main Menu")
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-#Image var
-arrowright = ImageTk.PhotoImage(Image.open("arrowright.png"))
-arrowleft = ImageTk.PhotoImage(Image.open("arrowleft.png"))
-PacMan = ImageTk.PhotoImage(Image.open("PacMan.png"))
-backroom = ImageTk.PhotoImage(Image.open("PacMan.png"))
-Mario = ImageTk.PhotoImage(Image.open("smbNES.jpg"))
+# Load images for the games
+pacman_image = pygame.image.load("PacMan.png").convert_alpha()
+mario_image = pygame.image.load("smbNES.jpg").convert_alpha()
+donkeykong_image = pygame.image.load("dkong.jpg").convert_alpha()
 
-#Image background:
-bg = Image.open("backgroundImage.ppm")
-resized_image= bg.resize((screen_width,screen_height), Image.ANTIALIAS)
-tk_img = ImageTk.PhotoImage(resized_image)
-#Label Canvas base :
-label= Label(root, text= "Hello World!", font=('Times New Roman bold',20))
-label = ttk.Label(root, image=tk_img)
-label.place(x=0, y=0)
+# Create a list of games
+games = [("Pacman", pacman_image), ("Mario Nes", mario_image), ("DK Nes", donkeykong_image)]
 
+# Set the font for the menu text
+font = pygame.font.Font(None, 50)
 
-btnARROWLEFT = Button(root, image = arrowleft, command=pacmancmd)
-btnARROWLEFT.place(x=0, y=350)  
-btnARROWRIGHT = Button(root, image = arrowright, command=pacmancmd)
-btnARROWRIGHT.place(x=1680, y=350)  
-btnPACMAN = Button(root, image = PacMan, command=pacmancmd)
-btnPACMAN.place(x=405, y=387)  
-btnBACKROOM = Button(root, image = backroom, command=backroomcmd)
-btnBACKROOM.place(x=810, y=387)  
-btnMARIO = Button(root, image = Mario, command=mariocmd)
-btnMARIO.place(x=1215, y=387)  
+# Set the initial selection to the first game
+selected = 0
 
+# Loop until the user clicks the close button
+done = False
+while not done:
+    # --- Main event loop ---
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                done = True
+            elif event.key == pygame.K_LEFT:
+                pygame.mixer.Channel(0).play(pygame.mixer.Sound("click.wav"), maxtime=600)
+                # Move the selection to the left
+                selected = max(selected - 1, 0)
+            elif event.key == pygame.K_RIGHT:
+                pygame.mixer.Channel(0).play(pygame.mixer.Sound("click.wav"), maxtime=600)
+                # Move the selection to the right
+                selected = min(selected + 1, len(games) - 1)
+            elif event.key == pygame.K_RETURN:
+                pygame.mixer.Channel(0).play(pygame.mixer.Sound("pickupGame.wav"), maxtime=600)
+                # Launch the selected game
+                game_name, game_image = games[selected]
+                print(f"Launching {game_name}...")
+                if game_name == "Pacman":
+                    print(f"{game_name} Launched with success")
+                    #code to launch game
+                elif game_name == "Mario Nes":
+                    print(f"{game_name} Launched with success")
+                    #code to launch game
+                elif game_name == "DK Nes":
+                    print(f"{game_name} Launched with success")
+                    #code to launch game
+                else :
+                    print(f"Error, no game selectionned")
 
+    # --- Drawing code goes here ---
+    
+    # Blit the background image onto the screen
+    screen.blit(background_image, (0, 0))
 
-label.pack(padx=0, pady=0)
-frm = ttk.Frame(root, padding=10)
-root.attributes('-fullscreen', True)
+    # Draw the games
+    for i, game in enumerate(games):
+        game_name, game_image = game
+        x = 200 + (i * 450)
+        y = 200
+        # Draw the selection glow
+        if i == selected:
+            pygame.draw.rect(screen, GREEN, (x + 150, y + 150, 300, 300), 5)
+        # Resize the game image to fit inside the selection glow
+        game_image = pygame.transform.scale(game_image, (280, 280))
+        # Draw the game image inside the selection glow
+        screen.blit(game_image, (x + 160, y + 160))
 
+    # Update the screen
+    pygame.display.flip()
 
-
-
-ttk.Label(frm, text="Arcadia main menu").grid(column=0, row=0)
-root.mainloop()
+# Close the window and quit
+pygame.quit()
